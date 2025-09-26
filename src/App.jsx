220de958d097f36ddacc14be7c16fa85cab5338c
Santix <santiago_ivan_copaexample@hotmail.com>
2025-09-26 19:20:22 -0300

    componente contenedor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import Modal from './components/Modal'
-
+import { useState } from "react";
+import Modal from "./components/Modal";
+import Contenedor from "./components/Contenerdor";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <Contenedor />
       <Modal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
